cad5a968b1489aa75616f5aa22c7ed249199095d
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-11-03 20:28:39 +0200

    Iterate sprite x,y value from JSON file

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::error::Error;
 use std::string::String;
 use std::path::Path;
+use std::vec::Vec;
 use json::{JsonValue, parse};
 
 fn read_sprite_file(filename: &str) -> String {
@@ -18,10 +19,24 @@ fn read_sprite_file(filename: &str) -> String {
   }
 }
 
-pub fn load_character() -> JsonValue {
+pub fn load_character() {
   let character_json = read_sprite_file("./assets/character.json");
-  match parse(&character_json) {
+  let character = match parse(&character_json) {
     Ok(res) => res,
     Err(e) => panic!("Character JSON parse error {:?}", e),
+  };
+  let mut sprite_names = Vec::with_capacity(14);
+  for x in 1..15 {
+    let i = if x < 10 { format!("0{}", x) } else { format!("{}", x) };
+    sprite_names.push(format!("Jog_45_{}", i));
+    sprite_names.push(format!("Jog_135_{}", i));
+    sprite_names.push(format!("Jog_225_{}", i));
+    sprite_names.push(format!("Jog_315_{}", i));
+  }
+  for &ref sprite in &sprite_names {
+    let x = character[sprite]["frame"]["x"].as_f64();
+    let y = character[sprite]["frame"]["y"].as_f64();
+    println!("{} x: {:?}, y: {:?}", sprite, x, y);
   }
 }
+
