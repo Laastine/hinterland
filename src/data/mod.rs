ed0f95db06b7c9aad7103e5dc0ae178137c21f9f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-01-09 20:29:37 +0200

    Load fire sprites

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -21,8 +21,9 @@ fn read_sprite_file(filename: &str) -> String {
 }
 
 pub fn load_character() -> Vec<Rectangle> {
-  let mut sprites = Vec::with_capacity(256);
-  let mut sprite_names = Vec::with_capacity(256);
+  let mut sprites = Vec::with_capacity(512);
+  let mut move_sprite_names = Vec::with_capacity(256);
+  let mut fire_sprite_names = Vec::with_capacity(256);
   let character_json = read_sprite_file("./assets/character.json");
   let character = match parse(&character_json) {
     Ok(res) => res,
@@ -30,14 +31,33 @@ pub fn load_character() -> Vec<Rectangle> {
   };
   for x in 0..15 {
     for y in 0..13 {
-      sprite_names.push(format!("run_{}_{}", x, y));
+      move_sprite_names.push(format!("run_{}_{}", x, y));
     }
   }
-  for &ref sprite in &sprite_names {
-    let x = character["frames"][sprite]["frame"]["x"].as_f64().unwrap();
-    let y = character["frames"][sprite]["frame"]["y"].as_f64().unwrap();
-    let w = character["frames"][sprite]["frame"]["w"].as_f64().unwrap();
-    let h = character["frames"][sprite]["frame"]["h"].as_f64().unwrap();
+  for x in 0..15 {
+    for y in 0..3 {
+      fire_sprite_names.push(format!("fire_{}_{}", x, y));
+    }
+  }
+
+  for &ref move_sprite in &move_sprite_names {
+    let x = character["frames"][move_sprite]["frame"]["x"].as_f64().unwrap();
+    let y = character["frames"][move_sprite]["frame"]["y"].as_f64().unwrap();
+    let w = character["frames"][move_sprite]["frame"]["w"].as_f64().unwrap();
+    let h = character["frames"][move_sprite]["frame"]["h"].as_f64().unwrap();
+    sprites.push(Rectangle {
+      w: w,
+      h: h,
+      x: x,
+      y: y,
+    });
+  }
+
+  for &ref fire_sprite in &fire_sprite_names {
+    let x = character["frames"][fire_sprite]["frame"]["x"].as_f64().unwrap();
+    let y = character["frames"][fire_sprite]["frame"]["y"].as_f64().unwrap();
+    let w = character["frames"][fire_sprite]["frame"]["w"].as_f64().unwrap();
+    let h = character["frames"][fire_sprite]["frame"]["h"].as_f64().unwrap();
     sprites.push(Rectangle {
       w: w,
       h: h,
