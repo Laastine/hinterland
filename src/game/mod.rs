647aa0e7a26c3c0ebe72c471e45067c5f508953a
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-01-11 19:57:25 +0200

    Reindent game/mod.rs

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -45,60 +45,59 @@ pub enum ViewAction {
 }
 
 pub trait View {
-    fn render(&mut self, context: &mut Game, elapsed: f64) -> ViewAction;
-  }
+  fn render(&mut self, context: &mut Game, elapsed: f64) -> ViewAction;
+}
 
-  pub fn spawn<F>(title: &str, init: F)
-  where F: Fn(&mut Game) -> Box<View> {
-    let sdl_context = ::sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    let mut timer = sdl_context.timer().unwrap();
-    let _image_context = ::sdl2_image::init(::sdl2_image::INIT_PNG).unwrap();
+pub fn spawn<F>(title: &str, init: F) where F: Fn(&mut Game) -> Box<View> {
+  let sdl_context = ::sdl2::init().unwrap();
+  let video = sdl_context.video().unwrap();
+  let mut timer = sdl_context.timer().unwrap();
+  let _image_context = ::sdl2_image::init(::sdl2_image::INIT_PNG).unwrap();
 
-    let window = video.window(title, 1280, 720)
+  let window = video.window(title, 1280, 720)
     .position_centered().opengl().resizable()
     .build().unwrap();
 
-    let mut context = Game::new(
-      Events::new(sdl_context.event_pump().unwrap()),
-      window.renderer()
+  let mut context = Game::new(
+    Events::new(sdl_context.event_pump().unwrap()),
+    window.renderer()
       .accelerated()
       .build().unwrap());
 
-    let mut current_view = init(&mut context);
+  let mut current_view = init(&mut context);
 
-    let interval = 1_000 / 60;
-    let mut before = timer.ticks();
-    let mut last_second = timer.ticks();
-    let mut fps = 0u16;
+  let interval = 1_000 / 60;
+  let mut before = timer.ticks();
+  let mut last_second = timer.ticks();
+  let mut fps = 0u16;
 
-    loop {
-      let now = timer.ticks();
-      let dt = now - before;
-      let elapsed = dt as f64 / 1_000.0;
+  loop {
+    let now = timer.ticks();
+    let dt = now - before;
+    let elapsed = dt as f64 / 1_000.0;
 
-      if dt < interval {
-        timer.delay(interval - dt);
-        continue;
-      }
+    if dt < interval {
+      timer.delay(interval - dt);
+      continue;
+    }
 
-      before = now;
-      fps += 1;
+    before = now;
+    fps += 1;
 
-      if now - last_second > 1_000 {
-        println!("FPS: {}", fps);
-        last_second = now;
-        fps = 0;
-      }
+    if now - last_second > 1_000 {
+      println!("FPS: {}", fps);
+      last_second = now;
+      fps = 0;
+    }
 
-      context.events.pump(&mut context.renderer);
+    context.events.pump(&mut context.renderer);
 
-      match current_view.render(&mut context, elapsed) {
-        ViewAction::None => context.renderer.present(),
+    match current_view.render(&mut context, elapsed) {
+      ViewAction::None => context.renderer.present(),
 
-        ViewAction::Quit => break,
+      ViewAction::Quit => break,
 
-        ViewAction::ChangeView(new_view) => current_view = new_view,
-      }
+      ViewAction::ChangeView(new_view) => current_view = new_view,
     }
   }
+}
