7d5139bfabf5479e5b4ad786b60cf39881cba954
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-01-12 21:57:31 +0200

    Fix few warnings in event.rs

--- src/game/events.rs
+++ src/game/events.rs
@@ -48,11 +48,10 @@ macro_rules! struct_events {
 
         for event in self.pump.poll_iter() {
           use sdl2::event::Event::*;
-          use sdl2::event::WindowEvent;
           use sdl2::keyboard::Keycode::*;
 
           match event {
-            Window { win_event: Resized, .. } => {
+            Window { win_event: resized, .. } => {
               self.now.resize = Some(renderer.output_size().unwrap());
             },
 
