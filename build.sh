950ce96837e66f659fa0ace6f845d904bc4a6611
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-02-11 17:55:25 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 cargo build --release
 mkdir -p hacknslash
-cp assets/*.png hacknslash
-cp assets/*.json hacknslash
-cp -r assets/maps hacknslash
+mkdir -p hacknslash/assets
+cp assets/*.png hacknslash/assets
+cp assets/*.json hacknslash/assets
+cp -r assets/maps hacknslash/assets
+cp -r assets/audio hacknslash/assets
 cp target/release/hacknslash hacknslash
 
 tar zcfv hacknslash.tgz hacknslash
