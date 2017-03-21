8e96c711be78103ea11eee61b2e4e35cd1d4ff58
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-03-21 21:49:11 +0200

    Remove old view

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,16 +1,24 @@
-use sdl2::mixer::{Chunk};
-use std::path::Path;
-use std::mem::replace;
+#[macro_use]
+use game::gfx_macros;
+use gfx;
+use gfx_app;
+use gfx::{Resources};
+use gfx_app::{Application, WindowTargets};
+use cgmath::{Transform, Point3, Vector3};
+use cgmath::{SquareMatrix, Matrix4, AffineMatrix3};
+
+use winit;
+use winit::VirtualKeyCode as Key;
+use winit::Event::KeyboardInput;
+use winit::ElementState::Pressed;
+use std::process;
 use std::fmt::{Display, Formatter, Result};
-use game::{Game, View, ViewAction};
-use game::data::{Rectangle, MaybeAlive};
-use game::gfx::{CopySprite, Sprite};
+
+use game::gfx_macros::{pipe, VertexData, TileMapData};
+use game::constants::MAP_FILE_PATH;
+use game::graphics::{TileMapPlane};
 use game::constants::{BACKGROUND_PATH, PISTOL_AUDIO_PATH, TILES_PCS_W, TILES_PCS_H, PLAYER_SPEED, ZOOM_SPEED};
-use views::tilemap::{TerrainTile, TerrainSpriteSheet, get_tiles, viewport_move, mapGlobalCoordinatesToGame};
-use views::background::{Background};
-use views::character::{Character, Stance};
-use views::zombie::{Zombie};
-use views::bullet::{Projectile};
+use data::{load_map_file, get_map_tile};
 
 mod bullet;
 mod character;
@@ -51,143 +59,6 @@ impl Display for Orientation {
     }
   }
 }
-
-pub struct GameView {
-  character: Character,
-  bullets: Vec<Box<Projectile>>,
-  tiles: Vec<TerrainTile>,
-  sprite_sheet: Vec<Sprite>,
-  background: Background,
-  zombies: Vec<Zombie>,
-  pistol: Chunk,
-  index: usize,
-}
-
-impl GameView {
-  pub fn new(game: &mut Game) -> GameView {
-    let pistol_audio = match Chunk::from_file(Path::new(PISTOL_AUDIO_PATH)) {
-      Ok(f) => f,
-      Err(e) => panic!("File {} not found: {}", PISTOL_AUDIO_PATH, e),
-    };
-
-    GameView {
-      character: Character::new(&mut game.renderer),
-      tiles: get_tiles(),
-      sprite_sheet: TerrainSpriteSheet::new(&game),
-      pistol: pistol_audio,
-      zombies: vec![Zombie::new(&mut game.renderer)],
-      background: Background {
-        pos: 0.0,
-        sprite: Sprite::load(&mut game.renderer, BACKGROUND_PATH).unwrap(),
-      },
-      index: 0,
-      bullets: vec![],
-    }
-  }
-}
-
-impl View for GameView {
-  fn render(&mut self, game: &mut Game, elapsed: f64) -> ViewAction {
-    let movable_region = Rectangle {
-      x: 0.0,
-      y: 0.0,
-      w: game.output_size().0,
-      h: game.output_size().1,
-    };
-
-    self.background.render(&mut game.renderer);
-
-    self.character.rect = self.character.rect.move_inside(movable_region).unwrap();
-
-    for x in 0..TILES_PCS_W {
-      for y in 0..TILES_PCS_H {
-        let index = x * TILES_PCS_H + y;
-        game.renderer.copy_sprite(&self.sprite_sheet[(self.tiles[index].current - 1) as usize], self.tiles[index].rect);
-      }
-    }
-
-    self.bullets =
-      replace(&mut self.bullets, vec![])
-        .into_iter()
-        .filter_map(|b| b.update(game, elapsed))
-        .collect();
-
-    let mut transition_bullets: Vec<_> =
-      replace(&mut self.bullets, vec![])
-        .into_iter()
-        .map(|b| MaybeAlive { alive: true, value: b })
-        .collect();
-
-    self.zombies =
-      replace(&mut self.zombies, vec![])
-        .into_iter()
-        .filter_map(|z| {
-          let mut zombie_alive = true;
-
-          for bullet in &mut transition_bullets {
-            if z.rect().overlaps(bullet.value.rect()) {
-              zombie_alive = false;
-            }
-          }
-
-          if zombie_alive {
-            Some(z)
-          } else {
-            None
-          }
-        })
-        .collect();
-
-    self.bullets = transition_bullets.into_iter()
-      .filter_map(MaybeAlive::as_option)
-      .collect();
-
-    self.zombies = replace(&mut self.zombies, vec![])
-      .into_iter()
-      .filter_map(|z| z.update(elapsed))
-      .collect();
-
-    for zombie in &mut self.zombies {
-      zombie.render(&mut game.renderer);
-    }
-
-    self.character.render(&mut game.renderer);
-
-    let point = mapGlobalCoordinatesToGame(self.character.rect.x, self.character.rect.y);
-    println!("point {:?}", point);
-
-
-    for bullet in &self.bullets {
-      bullet.render(game);
-    }
-
-    let scale = game.renderer.scale();
-
-    ViewAction::None
-  }
-}
-
-////////////////
-
-#[macro_use]
-use game::gfx_macros;
-use gfx;
-use gfx_app;
-use gfx::{Resources};
-use gfx_app::{Application, WindowTargets};
-use cgmath::{Transform, Point3, Vector3};
-use cgmath::{SquareMatrix, Matrix4, AffineMatrix3};
-
-use winit;
-use winit::VirtualKeyCode as Key;
-use winit::Event::KeyboardInput;
-use winit::ElementState::Pressed;
-use std::process;
-
-use game::gfx_macros::{pipe, VertexData, TileMapData};
-use game::constants::MAP_FILE_PATH;
-use game::graphics::{TileMapPlane};
-use data::{load_map_file, get_map_tile};
 
 #[derive(Clone)]
 struct InputState {
