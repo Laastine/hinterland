fab6ec058772df2c4c9b4de39732fa405db583d1
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-07-28 20:55:03 +0300

    Add Position struct for character

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,13 +1,13 @@
-use terrain::gfx_macros::{Projection};
+use terrain::gfx_macros::Projection;
 use cgmath;
-use cgmath::{Matrix4, Point3, Vector3};
+use cgmath::{Matrix4, Point3, Vector3, Point2, Deg};
 use specs;
 use terrain;
 
 pub type Delta = f32;
 pub type Planner = specs::Planner<Delta>;
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct Dimensions {
   width: u32,
   height: u32,
@@ -34,4 +34,31 @@ impl Dimensions {
       proj: cgmath::perspective(cgmath::Deg(60.0f32), aspect_ratio, 0.1, 4000.0).into(),
     }
   }
+}
+
+#[derive(Debug, Clone)]
+pub struct Position {
+  pub position: Point2<f32>,
+  pub orientation: Deg<f32>,
+  pub scale: f32,
+}
+
+impl Position {
+  pub fn new(x: f32, y: f32, angle: Deg<f32>, scale: f32) -> Position {
+    Position {
+      position: Point2::new(x, y),
+      orientation: angle,
+      scale: scale,
+    }
+  }
+
+  pub fn model_to_world(&self) -> Matrix4<f32> {
+    Matrix4::from_translation(Vector3::new(self.position.x, self.position.y, 0.0)) *
+      Matrix4::from_nonuniform_scale(self.scale, self.scale, 1.0) *
+      Matrix4::from_angle_z(-self.orientation)
+  }
+}
+
+impl specs::Component for Position {
+  type Storage = specs::VecStorage<Position>;
 }
