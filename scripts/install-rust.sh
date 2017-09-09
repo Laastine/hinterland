0c26800fe32ebf2c45a9f63fae970500601c4dd7
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-09-09 09:08:46 +0300

    Update install-rust.sh

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -8,6 +8,5 @@ if [ -d $RUST_HOME ]; then
   echo "Using cached Rust version $VERSION at $RUST_HOME"
 else
   echo "Installing Rust using rustup.sh"
-  curl -sSf https://static.rust-lang.org/rustup.sh | \
-      sh -s -- --channel=stable --disable-sudo
+  curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- --channel=stable
 fi
