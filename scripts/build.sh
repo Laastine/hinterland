296bfad45489032e1d6bea93c499eea51d3071d5
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-10-17 20:03:08 +0300

    Move build.sh to scripts folder

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd .. && cargo build --release && popd
+mkdir -p zombie-shooter
+mkdir -p zombie-shooter/assets
+cp ../assets/*.png zombie-shooter/assets
+cp ../assets/*.json zombie-shooter/assets
+cp -r ../assets/maps zombie-shooter/assets
+cp -r ../assets/audio zombie-shooter/assets
+cp ../target/release/zombie-shooter zombie-shooter
+find zombie-shooter -name '.DS_Store' -type f -delete
+
+tar zcfv zombie-shooter.tgz zombie-shooter
+rm -r zombie-shooter
+
+echo 'done'
