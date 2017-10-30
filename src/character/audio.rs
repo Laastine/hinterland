f7c6d185498a8bfc5cf63669b1e2d62a2ca90b4a
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-10-30 19:52:32 +0200

    Clean up constants file

--- src/character/audio.rs
+++ src/character/audio.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use rodio;
 use rodio::Sink;
+use game::constants::PISTOL_AUDIO_PATH;
 
 pub struct CharacterAudio {
   sink: Sink,
@@ -17,7 +18,7 @@ impl CharacterAudio {
 
   pub fn play_pistol(&self) {
     if self.sink.empty() {
-      let file = File::open("./assets/audio/pistol.ogg").unwrap();
+      let file = File::open(PISTOL_AUDIO_PATH).unwrap();
       let pistol_data = rodio::Decoder::new(BufReader::new(file)).unwrap();
       self.sink.append(pistol_data);
     }
--- src/game/constants.rs
+++ src/game/constants.rs
@@ -1,38 +1,5 @@
-//pub const PLAYER_SPEED: f64 = 170.0;
-//pub const ZOOM_SPEED: f32 = 0.01;
-//
-//pub const FIRE_SPRITE_START_INDEX: usize = 211;
-//
 pub const TILES_PCS_W: usize = 64;
 pub const TILES_PCS_H: usize = 64;
-//
-//pub const TILESHEET_PCS_W: usize = TILES_PCS_W - 1;
-//pub const TILESHEET_PCS_H: usize = TILES_PCS_H - 1;
-
-//pub const CHARACTER_W: f32 = 45.0;
-//pub const CHARACTER_H: f32 = 60.0;
-
-//pub const CHARACTER_POS_W: f64 = SCREEN_WIDTH * 0.5;
-//pub const CHARACTER_POS_H: f64 = SCREEN_HEIGHT * 0.4;
-//
-//pub const ZOMBIE_W: f64 = 56.0;
-//pub const ZOMBIE_H: f64 = 28.0;
-//
-//pub const ZOMBIE_POS_W: f64 = SCREEN_WIDTH * 0.7;
-//pub const ZOMBIE_POS_H: f64 = SCREEN_HEIGHT * 0.2;
-//
-//pub const BULLET_W: f64 = 2.0;
-//pub const BULLET_H: f64 = 2.0;
-//pub const BULLET_SPEED: f64 = 2000.0;
-
-//Assets
-//pub const ZOMBIE_PATH: &'static str = "assets/zombie.png";
-pub const ZOMBIE_JSON_PATH: &'static str = "assets/zombie.json";
-//pub const CHARACTER_PATH: &'static str = "assets/character.png";
-pub const CHARACTER_JSON_PATH: &'static str = "assets/character.json";
-//pub const PISTOL_AUDIO_PATH: &'static str = "assets/audio/pistol.ogg";
-pub const MAP_FILE_PATH: &'static str = "assets/maps/tilemap.tmx";
-pub const RUN_SPRITE_OFFSET: usize = 64;
 
 pub const TILEMAP_BUF_LENGTH: usize = 4096;
 pub const CHARACTER_BUF_LENGTH: usize = 224;
@@ -48,3 +15,10 @@ pub const CHARSHEET_TOTAL_WIDTH: f32 = 16_128f32;
 pub const SPRITE_OFFSET: f32 = 2.0;
 
 pub const ZOMBIESHEET_TOTAL_WIDTH: f32 = 7_872f32;
+
+//Assets
+pub const ZOMBIE_JSON_PATH: &'static str = "assets/zombie.json";
+pub const CHARACTER_JSON_PATH: &'static str = "assets/character.json";
+pub const PISTOL_AUDIO_PATH: &'static str = "assets/audio/pistol.ogg";
+pub const MAP_FILE_PATH: &'static str = "assets/maps/tilemap.tmx";
+pub const RUN_SPRITE_OFFSET: usize = 64;
