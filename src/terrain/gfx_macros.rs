2991a15ece7e0ea1673c4007ae138c5e65612fd2
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-11-14 19:52:42 +0200

    Reindent terrain/gfx_macros.rs

--- src/terrain/gfx_macros.rs
+++ src/terrain/gfx_macros.rs
@@ -2,27 +2,27 @@ use gfx;
 use shaders::Projection;
 
 gfx_defines! {
-    constant TileMapData {
-        data: [f32; 4] = "data",
-    }
+  constant TileMapData {
+      data: [f32; 4] = "data",
+  }
 
-    constant TilemapSettings {
-        world_size: [f32; 2] = "u_WorldSize",
-        tilesheet_size: [f32; 2] = "u_TilesheetSize",
-    }
+  constant TilemapSettings {
+      world_size: [f32; 2] = "u_WorldSize",
+      tilesheet_size: [f32; 2] = "u_TilesheetSize",
+  }
 
-    vertex VertexData {
-        pos: [f32; 3] = "a_Pos",
-        buf_pos: [f32; 2] = "a_BufPos",
-    }
+  vertex VertexData {
+      pos: [f32; 3] = "a_Pos",
+      buf_pos: [f32; 2] = "a_BufPos",
+  }
 
-    pipeline pipe {
-        vbuf: gfx::VertexBuffer<VertexData> = (),
-        projection_cb: gfx::ConstantBuffer<Projection> = "b_VsLocals",
-        tilemap: gfx::ConstantBuffer<TileMapData> = "b_TileMap",
-        tilemap_cb: gfx::ConstantBuffer<TilemapSettings> = "b_PsLocals",
-        tilesheet: gfx::TextureSampler<[f32; 4]> = "t_TileSheet",
-        out_color: gfx::RenderTarget<gfx::format::Rgba8> = "Target0",
-        out_depth: gfx::DepthTarget<gfx::format::DepthStencil> = gfx::preset::depth::LESS_EQUAL_WRITE,
-    }
+  pipeline pipe {
+      vbuf: gfx::VertexBuffer<VertexData> = (),
+      projection_cb: gfx::ConstantBuffer<Projection> = "b_VsLocals",
+      tilemap: gfx::ConstantBuffer<TileMapData> = "b_TileMap",
+      tilemap_cb: gfx::ConstantBuffer<TilemapSettings> = "b_PsLocals",
+      tilesheet: gfx::TextureSampler<[f32; 4]> = "t_TileSheet",
+      out_color: gfx::RenderTarget<gfx::format::Rgba8> = "Target0",
+      out_depth: gfx::DepthTarget<gfx::format::DepthStencil> = gfx::preset::depth::LESS_EQUAL_WRITE,
+  }
 }
