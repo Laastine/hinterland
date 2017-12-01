b2302365619993ccf9d75b6f6e03a40100e0549f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-01 21:44:45 +0200

    Update bullet size and color for debugging

--- src/bullet/mod.rs
+++ src/bullet/mod.rs
@@ -84,12 +84,12 @@ impl<R: gfx::Resources> BulletDrawSystem<R> {
 
     let vertex_data: Vec<VertexData> =
       vec![
-        VertexData::new([-0.5, -0.5, 0.0], [0.0, 1.0]),
-        VertexData::new([0.5, -0.5, 0.0], [1.0, 1.0]),
-        VertexData::new([0.5, 0.5, 0.0], [1.0, 0.0]),
-        VertexData::new([-0.5, -0.5, 0.0], [0.0, 1.0]),
-        VertexData::new([0.5, 0.5, 0.0], [1.0, 0.0]),
-        VertexData::new([-0.5, 0.5, 0.0], [0.0, 0.0]),
+        VertexData::new([-1.0, -1.0, 0.0], [0.0, 1.0]),
+        VertexData::new([1.0, -1.0, 0.0], [1.0, 1.0]),
+        VertexData::new([1.0, 1.0, 0.0], [1.0, 0.0]),
+        VertexData::new([-1.0, -1.0, 0.0], [0.0, 1.0]),
+        VertexData::new([1.0, 1.0, 0.0], [1.0, 0.0]),
+        VertexData::new([-1.0, 1.0, 0.0], [0.0, 0.0]),
       ];
 
     let (vertex_buf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, ());
--- src/shaders/bullet.f.glsl
+++ src/shaders/bullet.f.glsl
@@ -3,5 +3,5 @@
 out vec4 Target0;
 
 void main() {
-  Target0 = vec4(0.2, 0.2, 0.2, 0.8);
+  Target0 = vec4(0.5, 0.2, 0.2, 0.8);
 }
