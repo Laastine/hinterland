32740ee0fe320bb929189f500e9cde3d336c1aee
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-01-01 10:26:23 +0200

    Allow use of deprecated "get_default_endpoint"

--- src/character/audio.rs
+++ src/character/audio.rs
@@ -10,6 +10,7 @@ pub struct CharacterAudio {
 
 impl CharacterAudio {
   pub fn new() -> CharacterAudio {
+    #[allow(deprecated)]
     let endpoint = rodio::get_default_endpoint().unwrap();
     CharacterAudio {
       sink: Sink::new(&endpoint),
