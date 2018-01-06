a1f57120faa1c5bb9883b426220b62dcfdb309e0
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-01-06 21:55:56 +0200

    Add more direction calculation unit tests

--- tests/test.rs
+++ tests/test.rs
@@ -28,7 +28,7 @@ fn direction_test() {
   }, Point2 {
     x: 2.0,
     y: 3.0,
-  }), "(-2,1) to (2,3) should be 45deg");
+  }), "(-2,1) to (2,3) should be 26deg");
 
   assert_eq!(45, zombie_shooter_lib::graphics::direction(Point2 {
     x: -2.0,
@@ -53,4 +53,42 @@ fn direction_test() {
     x: 1.0,
     y: -4.0,
   }), "(-1,-2) to (1,-4) should be 315deg");
+}
+
+#[test]
+fn direction_movement_test() {
+  use cgmath::Point2;
+
+  assert_eq!(Point2 { x: 1.0, y: 0.0 },
+             zombie_shooter_lib::graphics::direction_movement(
+               zombie_shooter_lib::graphics::direction(Point2 {
+                 x: 1.0,
+                 y: 0.0,
+               }, Point2 {
+                 x: 2.0,
+                 y: 0.0,
+               })
+             ), "(1,0) to (2,0) should be (1,0)");
+
+  assert_eq!(Point2 { x: 0.0, y: 1.0 },
+             zombie_shooter_lib::graphics::direction_movement(
+               zombie_shooter_lib::graphics::direction(Point2 {
+                 x: 0.0,
+                 y: 1.0,
+               }, Point2 {
+                 x: 0.0,
+                 y: 2.0,
+               })
+             ), "(0,1) to (0,2) should be (0,1)");
+
+  assert_eq!(Point2 { x: 0.71, y: 0.71 }, // 0.71 = sqrt(2) / 2.0
+             zombie_shooter_lib::graphics::direction_movement(
+               zombie_shooter_lib::graphics::direction(Point2 {
+                 x: -2.0,
+                 y: -2.0,
+               }, Point2 {
+                 x: -1.0,
+                 y: -1.0,
+               })
+             ), "(-2,-2) to (-1,-1) should be 45deg");
 }
