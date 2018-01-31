ff81797ea299490d289499f44d5fb48d27ed6750
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-01-31 18:03:52 +0200

    Setup ALSA for Linux build in .travis.yml

--- scripts/install-alsa.sh
+++ scripts/install-alsa.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt-get remove --purge alsa-base pulseaudio
-sudo apt-get install alsa-base pulseaudio
-sudo alsa force-reload
-sudo apt install libasound2-dev
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+  sudo apt-get remove --purge alsa-base pulseaudio
+  sudo apt-get install alsa-base pulseaudio
+  sudo alsa force-reload
+  sudo apt install libasound2-dev
+fi
