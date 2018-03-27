94220aacaf5029db05744678c4f944ec33b70e2f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-03-27 11:10:37 +0300

    Do not warn about unused debug code

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -8,9 +8,10 @@ pub fn get_random_bool() -> bool {
   rnd.gen()
 }
 
-pub fn get_tenth_bool() -> bool {
+#[allow(dead_code)]
+pub fn get_weighted_random(weight: f32) -> bool {
   let mut rnd = rand::thread_rng();
-  rnd.gen::<f32>() < 0.05
+  rnd.gen::<f32>() < weight
 }
 
 
