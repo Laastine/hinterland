4ede6cee83b68413e3c3d8ee8739388271f1eec1
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-04-12 20:50:24 +0300

    Refactor more "load_texture" function

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -150,11 +150,10 @@ pub fn load_texture<R, F>(factory: &mut F, data: &[u8]) -> ShaderResourceView<R,
   let img = image::load(Cursor::new(data), image::PNG).unwrap().to_rgba();
   let (width, height) = img.dimensions();
   let kind = texture::Kind::D2(width as texture::Size, height as texture::Size, texture::AaMode::Single);
-  let (_, view) = match factory.create_texture_immutable_u8::<Rgba8>(kind, texture::Mipmap::Provided, &[&img]) {
-    Ok(val) => val,
-    Err(e) => panic!("Couldn't create texture {:?}", e)
-  };
-  view
+  match factory.create_texture_immutable_u8::<Rgba8>(kind, texture::Mipmap::Provided, &[&img]) {
+    Ok(val) => val.1,
+    Err(e) => panic!("Couldn't load texture {:?}", e)
+  }
 }
 
 pub enum Drawables {
