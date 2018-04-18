6c860ed62343d2cae451c5449e0b953e05f3377a
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-04-18 20:54:06 +0300

    Add "load_raw_texture" function

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -11,6 +11,7 @@ use shaders::{Position, Projection};
 use std::io::Cursor;
 use terrain_object::TerrainObjectDrawable;
 use zombie::ZombieDrawable;
+use gfx_app::ColorFormat;
 
 pub mod camera;
 pub mod orientation;
@@ -151,6 +152,17 @@ pub fn load_texture<R, F>(factory: &mut F, data: &[u8]) -> ShaderResourceView<R,
   let (width, height) = img.dimensions();
   let kind = texture::Kind::D2(width as texture::Size, height as texture::Size, texture::AaMode::Single);
   match factory.create_texture_immutable_u8::<Rgba8>(kind, texture::Mipmap::Provided, &[&img]) {
+    Ok(val) => val.1,
+    Err(e) => panic!("Couldn't load texture {:?}", e)
+  }
+}
+
+pub fn load_raw_texture<R, F>(factory: &mut F, data: &[u8], size: Point2<i32>) -> ShaderResourceView<R, [f32; 4]>
+                      where R: Resources, F: Factory<R> {
+  let kind = texture::Kind::D2(size.x as texture::Size, size.y as texture::Size, texture::AaMode::Single);
+  let mipmap = texture::Mipmap::Provided;
+  match factory
+    .create_texture_immutable_u8::<ColorFormat>(kind, mipmap, &[data]) {
     Ok(val) => val.1,
     Err(e) => panic!("Couldn't load texture {:?}", e)
   }
