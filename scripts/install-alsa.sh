7b65c95a9e4b4e1b0b41c34fdf151bc8217d8b63
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-05-11 22:02:28 +0300

    Remove scripts folder

--- scripts/install-alsa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-  sudo apt-get remove --purge alsa-base pulseaudio
-  sudo apt-get install alsa-base pulseaudio
-  sudo alsa force-reload
-  sudo apt install libasound2-dev
-fi
