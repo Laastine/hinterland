2aa7b86a8bf018b08f325cbcc0213f673b43240b
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-05-26 17:45:40 +0300

    Fix path finding logic

--- src/terrain/path_finding.rs
+++ src/terrain/path_finding.rs
@@ -5,16 +5,20 @@ use pathfinding::{astar::astar, utils::absdiff};
 use shaders::Position;
 
 fn tiles(p: &Point2<usize>) -> Vec<(Point2<usize>, usize)> {
-  let neighbours: Vec<Point2<usize>> = vec![Point2::new(p.x - 1, p.y),
-                                            Point2::new(p.x - 1, p.y - 1),
-                                            Point2::new(p.x, p.y - 1),
-                                            Point2::new(p.x + 1, p.y),
-                                            Point2::new(p.x + 1, p.y + 1),
-                                            Point2::new(p.x, p.y + 1)];
+  let e = Point2::new(p.x as i32, p.y as i32);
+  let neighbours: Vec<Point2<i32>> = vec![Point2::new(e.x - 1, e.y),
+                                          Point2::new(e.x - 1, e.y - 1),
+                                          Point2::new(e.x, e.y - 1),
+                                          Point2::new(e.x + 1, e.y),
+                                          Point2::new(e.x + 1, e.y + 1),
+                                          Point2::new(e.x, e.y + 1),
+                                          Point2::new(e.x - 1, e.y + 1),
+                                          Point2::new(e.x + 1, e.y - 1)
+  ];
 
   neighbours.iter()
-            .filter(|ref e| !TERRAIN_OBJECTS.contains(&[e.x, e.y]))
-            .map(|p| (Point2::new(p.x, p.y), 1))
+            .filter(|ref e| !TERRAIN_OBJECTS.contains(&[e.x as usize, e.y as usize]))
+            .map(|p| (Point2::new(p.x as usize, p.y as usize), 1))
             .collect()
 }
 
@@ -31,20 +35,26 @@ pub fn calc_route(start_point: Position, end_point: Position) -> Option<(Vec<Poi
 pub fn calc_next_movement(start_point: Position, end_point: Position) -> i32 {
   let next_step: Point2<i32> = calc_route(start_point, end_point)
     .map_or_else(|| Point2::new(0 as i32, 0 as i32),
-                 |(route, _)| Point2::new(route[1].x as i32, route[1].y as i32));
+                 |(route, _)| {
+                   if route.len() > 1 {
+                     Point2::new(route[1].x as i32, route[1].y as i32)
+                   } else {
+                     Point2::new(route[0].x as i32, route[0].y as i32)
+                   }
+                 });
 
   let start = coords_to_tile(start_point);
 
   let diff: (i32, i32) = (start.x as i32 - next_step.x, start.y as i32 - next_step.y);
   match diff {
-    (1, 0) => 0,
-    (1, 1) => 45,
-    (0, 1) => 180,
-    (-1, 1) => 315,
-    (-1, 0) => 270,
-    (-1, -1) => 225,
-    (0, -1) => 90,
-    (1, -1) => 135,
+    (1, 0) => 315,
+    (1, 1) => 270,
+    (0, 1) => 225,
+    (-1, 1) => 180,
+    (-1, 0) => 135,
+    (-1, -1) => 90,
+    (0, -1) => 45,
+    (1, -1) => 0,
     _ => 0,
   }
 }
