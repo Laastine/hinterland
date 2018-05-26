6ca86907660e81b02fee48841af3625bb4a30e91
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-05-26 20:07:12 +0300

    Add "get_rand_from_range" function

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -8,6 +8,11 @@ pub fn get_random_bool() -> bool {
   rnd.gen()
 }
 
+pub fn get_rand_from_range(min: i32, max: i32) -> i32 {
+  let mut rnd = rand::thread_rng();
+  rnd.gen_range(min, max)
+}
+
 #[allow(dead_code)]
 pub fn get_weighted_random(weight: f32) -> bool {
   let mut rnd = rand::thread_rng();
