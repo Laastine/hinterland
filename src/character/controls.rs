719b36896bc52a55979682ab5ef7885e37a73c2e
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-07-23 14:23:29 +0300

    Remove irrelevant linter ignore

--- src/character/controls.rs
+++ src/character/controls.rs
@@ -119,7 +119,6 @@ impl CharacterControlSystem {
 }
 
 impl<'a> specs::prelude::System<'a> for CharacterControlSystem {
-  #[cfg_attr(feature = "cargo-clippy", allow(type_complexity))]
   type SystemData = (WriteStorage<'a, CharacterInputState>,
                      ReadStorage<'a, CharacterDrawable>,
                      WriteStorage<'a, CameraInputState>,
