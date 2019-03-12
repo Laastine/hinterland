353fc8e70bcdfb856a40166382e3faeadf7e0db4
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2019-03-12 20:03:14 +0200

    Remove old source code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
 #[macro_use]
 extern crate gfx;
 
-mod audio;
-mod bullet;
-mod gfx_app;
-mod game;
-mod data;
-mod critter;
-pub mod graphics;
-mod hud;
-mod terrain_object;
-mod terrain;
-mod character;
-mod shaders;
 mod window;
-mod zombie;
 
 pub fn main() {
   let mut window = window::Window::new();
