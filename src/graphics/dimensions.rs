4b0a1060c7766466f69ef3f3f11794be484fe62f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2019-04-09 21:51:50 +0300

    Calculate projection in RHS

--- src/graphics/dimensions.rs
+++ src/graphics/dimensions.rs
@@ -3,6 +3,7 @@ use std::slice::from_raw_parts;
 
 use cgmath;
 use cgmath::{BaseFloat, Matrix4, Point3, Vector3};
+use nalgebra;
 use crate::graphics::camera::CameraInputState;
 
 #[derive(Debug, Default)]
@@ -48,19 +49,19 @@ impl Projection {
   }
 }
 
-pub fn get_projection(view: Matrix4<f32>, aspect_ratio: f32) -> Projection {
+pub fn get_projection(view: nalgebra::base::Matrix4<f32>, aspect_ratio: f32) -> Projection {
   Projection {
     model: view.into(),
     view: view.into(),
-    proj: cgmath::perspective(cgmath::Deg(75.0f32), aspect_ratio, 0.1, 4000.0).into(),
+    proj: nalgebra::Perspective3::new( aspect_ratio, 75.0f32, 0.1, 10000.0).into_inner().into(),
   }
 }
 
-pub fn get_view_matrix(view: f32) -> Matrix4<f32> {
-  Matrix4::look_at(
-    Point3::new(0.0, 0.0, view),
-    Point3::new(0.0, 0.0, 0.0),
-    Vector3::unit_y(),
+pub fn get_view_matrix(view: f32) -> nalgebra::Matrix4<f32> {
+  nalgebra::base::Matrix4::look_at_rh(
+    &nalgebra::Point3::new(0.0, 0.0, view),
+    &nalgebra::Point3::new(0.0, 0.0, 0.0),
+    &nalgebra::Vector3::new(0.0, 1.0, 0.0),
   )
 }
 
@@ -82,7 +83,7 @@ impl Dimensions {
   }
 
   pub fn world_to_projection(&self, input: &CameraInputState) -> Projection {
-    let view: Matrix4<f32> = get_view_matrix(input.distance);
+    let view= get_view_matrix(input.distance);
     let aspect_ratio = self.window_width / self.window_height;
     get_projection(view, aspect_ratio)
   }
