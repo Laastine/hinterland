614739e1e2e9742fbfdaf698c0e90dfa5864dcca
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2019-08-25 14:03:34 +0300

    Extract rectangle mesh to new function

--- src/graphics/mesh.rs
+++ src/graphics/mesh.rs
@@ -10,6 +10,15 @@ use crate::shaders::VertexData;
 
 const DEFAULT_INDEX_DATA: &[u16] = &[0, 1, 2, 2, 3, 0];
 
+fn rectangle_mesh(w: f32, h: f32) -> [VertexData; 4] {
+  [
+    VertexData::new([-w, -h], [0.0, 1.0]),
+    VertexData::new([w, -h], [1.0, 1.0]),
+    VertexData::new([w, h], [1.0, 0.0]),
+    VertexData::new([-w, h], [0.0, 0.0]),
+  ]
+}
+
 #[derive(Clone)]
 pub struct PlainMesh<R> where R: Resources {
   pub slice: gfx::Slice<R>,
@@ -30,12 +39,7 @@ impl<R> PlainMesh<R> where R: gfx::Resources {
     let h = size.y;
     let rot = rotation.unwrap_or(0.0);
 
-    let vertex_data: Vec<VertexData> = vec![
-      VertexData::new([-w, -h], [0.0, 1.0]),
-      VertexData::new([w, -h], [1.0, 1.0]),
-      VertexData::new([w, h], [1.0, 0.0]),
-      VertexData::new([-w, h], [0.0, 0.0]),
-    ].iter()
+    let vertex_data = rectangle_mesh(w, h).to_vec().iter()
       .map(|el| {
         let cos = Angle::cos(Deg(rot));
         let sin = Angle::sin(Deg(rot));
@@ -43,7 +47,7 @@ impl<R> PlainMesh<R> where R: gfx::Resources {
           Matrix2::<f32>::new(cos, -sin, sin, cos) * Vector2::<f32>::new(el.pos[0] as f32, el.pos[1] as f32);
         VertexData { pos: [rotated.x, rotated.y], uv: el.uv }
       })
-      .collect();
+      .collect::<Vec<VertexData>>();
 
     let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&vertex_data[..], DEFAULT_INDEX_DATA);
     PlainMesh {
@@ -70,13 +74,8 @@ impl<R> RectangularMesh<R> where R: gfx::Resources {
   pub fn new<F>(factory: &mut F, texture: Texture<R>, size: Point2<f32>) -> RectangularMesh<R> where F: gfx::Factory<R> {
     let w = size.x;
     let h = size.y;
-    let vertex_data: &[VertexData; 4] = &[
-      VertexData::new([-w, -h], [0.0, 1.0]),
-      VertexData::new([w, -h], [1.0, 1.0]),
-      VertexData::new([w, h], [1.0, 0.0]),
-      VertexData::new([-w, h], [0.0, 0.0]),
-    ];
-    let mesh = Mesh::new(factory, vertex_data, &DEFAULT_INDEX_DATA, texture);
+
+    let mesh = Mesh::new(factory, &rectangle_mesh(w, h), &DEFAULT_INDEX_DATA, texture);
     RectangularMesh {
       mesh,
       size,
